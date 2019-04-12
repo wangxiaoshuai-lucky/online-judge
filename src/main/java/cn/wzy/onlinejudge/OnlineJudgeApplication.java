bed118c210eca99164bd93a6f84b1bec0a0b4c9b
1510460325@qq.com <1510460325@qq.com>
2019-04-12 15:47:58 +0800

    配置以Cloud Provider身份启动

--- src/main/java/cn/wzy/onlinejudge/OnlineJudgeApplication.java
+++ src/main/java/cn/wzy/onlinejudge/OnlineJudgeApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-//@EnableEurekaClient
+@EnableEurekaClient
 @SpringBootApplication
 public class OnlineJudgeApplication extends SpringBootServletInitializer {
 
