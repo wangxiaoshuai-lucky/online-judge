a314aef22a10aacf9ad32be29d99b7abec3cded3
1510460325@qq.com <1510460325@qq.com>
2019-04-14 18:47:07 +0800

    添加日志系统

--- src/main/java/cn/wzy/onlinejudge/handler/Main.java
+++ src/main/java/cn/wzy/onlinejudge/handler/Main.java
@@ -1,7 +1,10 @@
 package cn.wzy.onlinejudge.handler;
 
+import lombok.extern.log4j.Log4j;
+
+@Log4j
 public class Main {
 	public static void main(String[] args) {
-		System.out.println(12 / 0);
+		log.info("asdf");
 	}
 }
