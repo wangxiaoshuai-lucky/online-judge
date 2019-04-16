30746508cbaa71cc3940fe40cfd25e40f308ba9c
1510460325@qq.com <1510460325@qq.com>
2019-04-16 15:44:03 +0800

    解决日志冲突问题

--- src/main/java/cn/wzy/onlinejudge/OnlineJudgeApplication.java
+++ src/main/java/cn/wzy/onlinejudge/OnlineJudgeApplication.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
+//@EnableEurekaClient
 @SpringBootApplication
 public class OnlineJudgeApplication extends SpringBootServletInitializer {
 
