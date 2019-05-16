c8116c80fed4a5debf9f5786a6da8fa73ead13af
1510460325@qq.com <1510460325@qq.com>
2019-05-16 16:45:47 +0800

    移植到阿里云服务器

--- producer/src/main/java/cn/wzy/producer/controller/JudgeController.java
+++ producer/src/main/java/cn/wzy/producer/controller/JudgeController.java
@@ -3,31 +3,32 @@ package cn.wzy.producer.controller;
 import cn.wzy.producer.vo.JudgeResult;
 import cn.wzy.producer.vo.JudgeTask;
 import com.alibaba.fastjson.JSON;
+import lombok.extern.log4j.Log4j;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
 @RestController
+@Log4j
 public class JudgeController {
 
 	@Resource
 	KafkaTemplate<String, String> kafkaTemplate;
 
-	@RequestMapping(value = "/judge.do", method = RequestMethod.POST)
+	@PostMapping("/judge.do")
 	public Object judge(@RequestBody JudgeTask task) {
 		kafkaTemplate.send("judge", JSON.toJSONString(task));
 		return "OK";
 	}
 
-	@RequestMapping(value = "/result.do", method = RequestMethod.PUT)
+	@PutMapping("/result.do")
 	public String result(String key, Long submitId, @RequestBody JudgeResult result) {
-		System.out.println(key);
-		System.out.println(submitId);
-		System.out.println(result);
+		log.info("*****************");
+		log.info("\tkey: " + key + "\n" +
+			"\tsubmitId: " + submitId + "\n" +
+			"\tresult: " + result);
+		log.info("*****************");
 		return "OK";
 	}
 
